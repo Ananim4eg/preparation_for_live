c4989704012decfc251de47b6d3e45687279de97
Ananim4eg <109145711+Ananim4eg@users.noreply.github.com>
2025-05-01 21:30:47 +0300

    Добавлена новая функция сортировки имен

--- src/get_names.py
+++ src/get_names.py
@@ -1,13 +1,16 @@
 import os
+import re
 
+PATH_TXT = os.path.join(
+    "C:\\Users\\EasyGod\\PycharmProjects\\PythonProject\\preparation_for_live\\", "data\\names.txt"
+)
+PATH_FOR_SAVE_TXT = "C:\\Users\\EasyGod\\PycharmProjects\\PythonProject\\preparation_for_live\\data\\"
 
-PATH_FOR_TXT = os.path.join("C:\\Users\EasyGod\PycharmProjects\PythonProject\preparation_for_live\\", "data\\names.txt")
+#Свой вариант
+def get_all_names_with_file_var1(path: str) -> str:
+    """Возвращает все имена из текстового файла"""
 
-
-def get_all_names_with_file(my_list: str) -> str:
-    """ Возвращает имена из текстового файла"""
-
-    with open(PATH_FOR_TXT, "r", encoding='utf-8') as file:
+    with open(path, "r", encoding="utf-8") as file:
 
         context = file.readlines()
         context_new = []
@@ -22,10 +25,47 @@ def get_all_names_with_file(my_list: str) -> str:
                     letters_for_del.append(elem)
 
             for item in letters_for_del:
-                modified_line = modified_line.replace(item,'')
+                modified_line = modified_line.replace(item, "")
 
             if len(modified_line) > 0:
                 context_new.append(modified_line)
 
-    return '\n'.join(context_new)
+    return "\n".join(context_new)
 
+
+#Вариант с гуглом
+def get_all_names_with_file_var2(path: str) -> str:
+    """Возвращает все имена из текстового файла"""
+
+    with open(path, "r", encoding="utf-8") as file:
+
+        context = file.read()
+        pattern = r"\b[а-я|a-z]*"
+
+        all_names = list(filter(None, re.findall(pattern, context, re.IGNORECASE)))
+
+    return "\n".join(all_names)
+
+
+def get_sort_names_with_file(path: str):
+    """Возвращает имена, сортируя их в алфавитном порядке и раскладывая по двум спискам ру. и анг."""
+
+    with open(path, "r", encoding="utf-8") as file:
+
+        context = file.read()
+        pattern_ru_names = r"\b[а-я]*"
+        pattern_eng_names = r"\b[a-z]*"
+
+    all_ru_names = list(filter(None, re.findall(pattern_ru_names, context, re.IGNORECASE)))
+
+    all_eng_names = list(filter(None, re.findall(pattern_eng_names, context, re.IGNORECASE)))
+
+    with open(PATH_FOR_SAVE_TXT + "ru_names.txt", 'w', encoding="utf-8") as ru_names:
+
+        ru_names.write('\n'.join(sorted(all_ru_names)))
+
+    with open(PATH_FOR_SAVE_TXT + "eng_names.txt", 'w', encoding="utf-8") as eng_names:
+
+        eng_names.write('\n'.join(sorted(all_eng_names)))
+
+get_sort_names_with_file(PATH_TXT)
